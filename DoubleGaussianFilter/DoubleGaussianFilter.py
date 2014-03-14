7639d6d88115aefe12d3a672ae4038862b0b1b42
Chris Meyer <cmeyer1969@gmail.com>
2014-03-14 11:53:44 -0700

    Make code PEP8 compliant.

--- DoubleGaussianFilter/DoubleGaussianFilter.py
+++ DoubleGaussianFilter/DoubleGaussianFilter.py
@@ -26,17 +26,15 @@ _ = gettext.gettext
 
 
 class DoubleGaussianFilterOperation(Operation.Operation):
-
     def __init__(self):
 
         # provide a description of the parameters to this operation. these parameters will
         # be placed in the processing panel as user interface elements.
-        description = [
-            { "name": _("Sigma 1"), "property": "sigma1", "type": "scalar", "default": 0.3 },
-            { "name": _("Sigma 2"), "property": "sigma2", "type": "scalar", "default": 0.3 },
-            { "name": _("Weight 2"), "property": "weight2", "type": "scalar", "default": 0.3 },
-        ]
-        super(DoubleGaussianFilterOperation, self).__init__(_("Double Gaussian Filter"), "double-gaussian-filter-operation", description)
+        description = [{"name": _("Sigma 1"), "property": "sigma1", "type": "scalar", "default": 0.3},
+                       {"name": _("Sigma 2"), "property": "sigma2", "type": "scalar", "default": 0.3},
+                       {"name": _("Weight 2"), "property": "weight2", "type": "scalar", "default": 0.3}, ]
+        super(DoubleGaussianFilterOperation, self).__init__(_("Double Gaussian Filter"),
+                                                            "double-gaussian-filter-operation", description)
         # initialize the parameters
         self.sigma1 = 0.3
         self.sigma2 = 0.3
@@ -65,17 +63,19 @@ class DoubleGaussianFilterOperation(Operation.Operation):
 
             # next, set up xx, yy arrays to be linear indexes for x and y coordinates ranging
             # from -width/2 to width/2 and -height/2 to height/2.
-            yy_min = int(math.floor(-data.shape[0]/2))
-            yy_max = int(math.floor(data.shape[0]/2))
-            xx_min = int(math.floor(-data.shape[1]/2))
-            xx_max = int(math.floor(data.shape[1]/2))
-            xx, yy = numpy.meshgrid(numpy.linspace(yy_min, yy_max, data.shape[0]), numpy.linspace(xx_min, xx_max, data.shape[1]))
+            yy_min = int(math.floor(-data.shape[0] / 2))
+            yy_max = int(math.floor(data.shape[0] / 2))
+            xx_min = int(math.floor(-data.shape[1] / 2))
+            xx_max = int(math.floor(data.shape[1] / 2))
+            xx, yy = numpy.meshgrid(numpy.linspace(yy_min, yy_max, data.shape[0]),
+                                    numpy.linspace(xx_min, xx_max, data.shape[1]))
 
             # calculate the pixel distance from the center
             rr = numpy.sqrt(numpy.square(xx) + numpy.square(yy)) / (data.shape[0] * 0.5)
 
             # finally, apply a filter to the Fourier space data.
-            filter = numpy.exp(-0.5*numpy.square(rr/sigma1)) - (1.0 - weight2) * numpy.exp(-0.5*numpy.square(rr/sigma2))
+            filter = numpy.exp(-0.5 * numpy.square(rr / sigma1)) - (1.0 - weight2) * numpy.exp(
+                -0.5 * numpy.square(rr / sigma2))
             filtered_fft_data = fft_data * filter
 
             # and then do invert FFT and take the real value.
@@ -87,13 +87,18 @@ class DoubleGaussianFilterOperation(Operation.Operation):
 
 
 # The following is code for making this into a process you can access from the processing menu
- 
+
 def processing_double_gaussian_filter(document_controller):
-    return document_controller.add_processing_operation_by_id("double-gaussian-filter-operation", prefix=_("Double Gaussian Filter of "))
+    return document_controller.add_processing_operation_by_id("double-gaussian-filter-operation",
+                                                              prefix=_("Double Gaussian Filter of "))
+
 
 def build_menus(document_controller):
-    document_controller.processing_menu.add_menu_item(_("Double Gaussian Filter"), lambda: processing_double_gaussian_filter(document_controller))
+    document_controller.processing_menu.add_menu_item(_("Double Gaussian Filter"),
+                                                      lambda: processing_double_gaussian_filter(document_controller))
+
 
 Application.app.register_menu_handler(build_menus)  # called on import to make the Show Color Phase Button
 
-Operation.OperationManager().register_operation("double-gaussian-filter-operation", lambda: DoubleGaussianFilterOperation())
+Operation.OperationManager().register_operation("double-gaussian-filter-operation",
+                                                lambda: DoubleGaussianFilterOperation())
